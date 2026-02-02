432c8aef33f2852f7c5108b48b0e47231221aeb0
Luan Lucino <109971413+LuanLucino@users.noreply.github.com>
2026-02-02 00:35:40 -0300

    Update rankingAnnouncements.js

--- src/utils/rankingAnnouncements.js
+++ src/utils/rankingAnnouncements.js
@@ -12,7 +12,7 @@ module.exports = function rankingAnnouncements(client) {
   setTimeout(() => {
 
     // Semanal: domingo 00:35
-    cron.schedule('35 0 * * 0', async () => {
+    cron.schedule('38 0 * * 1', async () => {
       console.log('[RANKING] Executando semanal...');
       await processWeekly(client);
     }, { timezone: 'America/Sao_Paulo' });
