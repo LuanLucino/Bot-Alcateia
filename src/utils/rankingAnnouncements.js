41f792cb69190ae5c9b0754ca689e8ed38e39124
Luan Lucino <109971413+LuanLucino@users.noreply.github.com>
2026-02-02 00:44:43 -0300

    Update rankingAnnouncements.js

--- src/utils/rankingAnnouncements.js
+++ src/utils/rankingAnnouncements.js
@@ -17,15 +17,12 @@ module.exports = function rankingAnnouncements(client) {
       await processWeekly(client);
     }, { timezone: 'America/Sao_Paulo' });
 
-    // Mensal: último dia do mês 23:59
-    cron.schedule('43 00 2 * *', async () => {
-      const today = new Date();
-      const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
-      if (today.getDate() === lastDay) {
-        console.log('[RANKING] Executando mensal...');
-        await processMonthly(client);
-      }
-    }, { timezone: 'America/Sao_Paulo' });
+    // Mensal: hoje, 2 de fevereiro às 00:50
+cron.schedule('50 0 2 2 *', async () => {
+  console.log('[RANKING] Executando mensal (forçado hoje)...');
+  await processMonthly(client);
+}, { timezone: 'America/Sao_Paulo' });
+
 
   }, 3000);
 };
